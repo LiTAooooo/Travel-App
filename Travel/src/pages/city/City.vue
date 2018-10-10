390a1bcb867512138ef2bd6dae47450579318aed
LiTAooooo <34028801+LiTAooooo@users.noreply.github.com>
2018-10-10 18:39:27 +0800

    Add city-list

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -2,12 +2,14 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header';
 import CitySearch from './components/Search';
+import CityList from './components/List';
 
 
 export default {
@@ -15,6 +17,7 @@ export default {
   components: {
     CityHeader,
     CitySearch,
+    CityList,
   },
 };
 </script>
