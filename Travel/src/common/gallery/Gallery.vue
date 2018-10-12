82c4c0587c0bf547b041b425988a82ba35b61356
LiTAooooo <34028801+LiTAooooo@users.noreply.github.com>
2018-10-12 23:25:17 +0800

    Add detail-banner

---
+++ Travel/src/common/gallery/Gallery.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container" @click="handleGalleryClick">
+        <swiper :options="swiperOption">
+            <swiper-slide
+              v-for="(img, index) of imgs"
+              :key="index"
+            >
+                <div class="img-wrapper">
+                    <img class="gallery-img" :src="img" />
+                </div>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: Array,
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents:true,
+        observer:true,
+      },
+    };
+  },
+  methods: {
+    handleGalleryClick() {
+      this.$emit('closeGallery');
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: visible
+    width: 100%
+  .container
+    display: flex
+    align-items: center
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background: #000
+    .swiper-pagination
+      bottom: -.6rem
+      color: #fff
+    .img-wrapper
+      position: relative
+      height: 0
+      padding-bottom: 100%
+      .gallery-img
+        width: 100%
+        position: absolute
+        top: 0
+        bottom: 0
+        margin: auto
+</style>
---
+++ Travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+        <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1412/89/66dadfb4d982e6f88abad45a3e062342.water.jpg_600x330_4e043224.jpg" />
+        <div class="banner-info">
+            <div class="banner-title">曲江海洋极地公园(AAAA景区)</div>
+            <div class="banner-num">
+                <span class="iconfont banner-icon">&#xe600;</span>{{this.imgs.length}}
+            </div>
+        </div>
+        <div class="bg-gradient"></div>
+    </div>
+    <common-gallery
+      v-show="showGallery"
+      @closeGallery="handleCloseGallery"
+      :imgs="imgs"
+    ></common-gallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/Gallery';
+
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallery,
+  },
+  data() {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_r_800x800_e9c1d2be.jpg', 'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_r_800x800_56e729ef.png'],
+    };
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallery = true;
+    },
+    handleCloseGallery() {
+      this.showGallery = false;
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      justify-content: space-between
+      align-items: center
+      position: absolute
+      left: .2rem
+      right: .2rem
+      bottom: .36rem
+      color: #fff
+      .banner-title
+        font-size: .36rem
+      .banner-num
+        width: 1.2rem
+        height: .4rem
+        line-height: .4rem
+        text-align: center
+        font-size: .24rem
+        background: rgba(0, 0, 0, .5)
+        border-radius: .2rem
+        .banner-icon
+          font-size: .24rem
+          margin-right: .1rem
+    .bg-gradient
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .6rem
+      background: linear-gradient(
+                    to bottom, 
+                    rgba(0, 0, 0, 0),
+                    rgba(0, 0, 0, .8)
+                  )
+</style>
